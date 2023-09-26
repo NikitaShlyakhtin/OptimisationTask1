7146d6793755324c388183fa482d101a4f5a144c
AruzhanShinbayeva <a.shinbaeyeva@innopolis.university>
2023-09-26 17:42:51 +0300

    testRunner

--- src/TestRunner.java
+++ src/TestRunner.java
@@ -1,6 +1,6 @@
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
-;
+
 public class TestRunner {
     public static void main(String[] args) {
         Result result = JUnitCore.runClasses(SimplexMethodTest.class);
