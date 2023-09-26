31a0ce29fa5163038b0faf22e9dad2ec1a5258a8
AruzhanShinbayeva <a.shinbaeyeva@innopolis.university>
2023-09-26 17:42:22 +0300

    added TestRunner

---
+++ src/TestRunner.java
@@ -0,0 +1,11 @@
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+;
+public class TestRunner {
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(SimplexMethodTest.class);
+        System.out.println("Total number of tests " + result.getRunCount());
+        System.out.println("Total number of tests failed " + result.getFailureCount());
+    }
+
+}
